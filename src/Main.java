c77999ec974c517078260821de2eb9890a48b058
Anna Maywald <aniamaywald@gmail.com>
2026-02-11 14:32:48 +0100

    Initial Commit - main

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+//          public static void main(String[] args) {
+//            Cinema cinema1 = new Cinema("Super Tarasy", "ul. Akademicka 5");
+//            //... configuration and test data should be inserted here...
+//            // below you will find sample function executions
+//            cinema1.printProgramme();
+//            Screening screening = cinema1.getScreenings()[0];
+//            screening.reservePlaces("H34", "H35", "H36"); // seats number given
+//            screening.reservePlaces(seat1, seat2, seat3); // other option
+//            screening.reservePlaces(customer, "H34", "H35", "H36"); // reservation for registered customer
+//            movie1 = cinema1.findMovie("James Bon");
+//            // ... etc ...
+//        }
+//    }
+
